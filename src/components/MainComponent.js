bc03abef1a6efa968bc0537f2634402bc71b4505
SOCRATE85 <siekapets@gmail.com>
2022-04-20 16:04:16 -0400

    Update on the About and Main page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from "./ContactComponent";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -64,6 +65,11 @@ class Main extends Component {
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </switch>
         <Footer />
